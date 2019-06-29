6f28629e1b65ea89e037893271d629a7fec8a3c5
Piotr <piotrkaszuba1996@gmail.com>
2019-06-29 15:35:23 +0200

    lr

--- linearRegression.py
+++ linearRegression.py
@@ -43,7 +43,7 @@ for column in x_train.columns:
 x_train, mean, std = dp.standardize(x_train, ret_mean_std=True)
 x_test = dp.standardize(x_test, mean, std)
 #x_test = x_test.dropna()
-pca = PCA(n_components=len(x_train.columns)-25)
+pca = PCA(n_components=28)
 x_train = pca.fit_transform(x_train)
 x_train = pd.DataFrame(data = x_train)
 x_test = pca.transform(x_test)
@@ -61,10 +61,10 @@ print(x_train.columns)
 # for i in range(0,len(x_train.columns),3):
 #     plt.annotate("{0:.2f}".format(np.cumsum(pca.explained_variance_ratio_)[i]), (i+1, np.cumsum(pca.explained_variance_ratio_)[i]))
 #     plt.axvline(x=i+1)
-#
+# #
 # plt.xticks( list(range(1,len(x_train.columns)+1,3)))
 # plt.show()
-poly = PolynomialFeatures(degree=2)
+poly = PolynomialFeatures(degree=3)
 x_train = poly.fit_transform(x_train)
 x_test = pd.DataFrame(poly.fit_transform(x_test))
 
@@ -76,8 +76,12 @@ x_test = pd.DataFrame(poly.fit_transform(x_test))
 
 lr.fit(x_train, y_train)
 print(lr.coef_)
+print(len(lr.coef_))
 pred=lr.predict(x_test)
 error = mean_absolute_error(y_test,pred) #calculate err
 r2 = r2_score(y_test, pred)
+
+AdjR2 = 1-(1-r2)*(200000-1)/(200000-len(lr.coef_)-1)
+print('AdjR2: ', AdjR2)
 print('MAE: ', error)
 print('R2: ', r2)
